3fca8e7bbaed24d62e90c61d38b3546d5938eb59
Caio Thomaz <caio@crthomaz.com.br>
2021-01-18 22:24:52 -0300

    Make everything integrant

--- src/vega/core.clj
+++ src/vega/core.clj
@@ -6,25 +6,31 @@
             [integrant.core :as ig]
             [java-time :as t]
             [morse.api :as api]
-            [morse.handlers :refer [defhandler command-fn message-fn]]
+            [morse.handlers :refer [handlers command-fn message-fn]]
             [morse.polling :as polling]
             [taoensso.timbre :as timbre])
   (:gen-class))
 
-(def db-config
-  {:store      {:backend :file
-                :path    "/tmp/vegadb"}
-   :initial-tx [{:db/ident       :friend/name
-                 :db/valueType   :db.type/string
-                 :db/cardinality :db.cardinality/one}
-                {:db/ident       :friend/zone-id
-                 :db/valueType   :db.type/string
-                 :db/cardinality :db.cardinality/one}]})
-
 (def config
-  {:database db-config})
+  {:telegram/polling {:token   (env :telegram-token)
+                      :handler (ig/ref :telegram/handler)}
 
-(defmethod ig/init-key :database [_ opts]
+   :telegram/handler {:token    (env :telegram-token)
+                      :db-setup (ig/ref :db/setup)}
+
+   :db/setup {:store      {:backend :file
+                           :path    "/tmp/vegadb"}
+              :initial-tx [{:db/ident       :friend/name
+                            :db/valueType   :db.type/string
+                            :db/cardinality :db.cardinality/one}
+                           {:db/ident       :friend/zone-id
+                            :db/valueType   :db.type/string
+                            :db/cardinality :db.cardinality/one}]
+              :name       "vegadb"}
+
+   :etc/logging {:level (keyword (env :log-level))}})
+
+(defmethod ig/init-key :db/setup [_ opts]
   (when-not (d/database-exists? opts)
     (d/create-database opts)
 
@@ -38,7 +44,10 @@
 
   opts)
 
-(def token (env :telegram-token))
+(defmethod ig/init-key :etc/logging [_ {:keys [level]
+                                        :or   {level :info}}]
+  (timbre/set-level! level))
+
 (def default-zone (t/zone-id "America/Sao_Paulo"))
 
 (defn now
@@ -46,8 +55,8 @@
   (t/zoned-date-time default-zone))
 
 (defn friend-time
-  [friend]
-  (let [conn      (d/connect db-config)
+  [db-setup friend]
+  (let [conn      (d/connect db-setup)
         [zone-id] (d/q '[:find [?z]
                          :in $ ?name
                          :where
@@ -58,47 +67,48 @@
               (t/with-zone-same-instant (now) (or zone-id
                                                   default-zone)))))
 
-(defhandler handler
-
-  (command-fn "start"
-    (fn [{{id :id :as chat} :chat}]
-      (println "Bot joined new chat: " chat)
-      (api/send-text token id "Vega initialized.")))
-
-  (command-fn "help"
-    (fn [{{id :id :as chat} :chat}]
-      (println "Help was requested in " chat)
-      (api/send-text token id "No.")))
-
-  (command-fn "time"
-    (fn [{:keys [text chat]}]
-      (api/send-text token (:id chat) (friend-time (second (s/split text #" "))))))
-
-  (message-fn
-    (fn [{:keys [text chat]}]
-      (when (s/includes? (s/lower-case text) "this is the way")
-        (api/send-text token (:id chat) "This is the way."))))
-
-  (message-fn
-    (fn [message]
-      (println "Intercepted message: " message)
-      (println "Not doing anything with this message."))))
-
-(defn start
-  [{:keys [log-level]
-    :or   {log-level :info}}]
-
-  (timbre/set-level! log-level)
-
+(defmethod ig/init-key :telegram/handler [_ {:keys [db-setup
+                                                    token]}]
   (when (s/blank? token)
     (timbre/info "Please provide token in TELEGRAM_TOKEN environment variable!")
     (System/exit 1))
 
-  (ig/init config)
+  (handlers
+    (command-fn "start"
+      (fn [{{id :id :as chat} :chat}]
+        (timbre/info "Bot joined new chat: " chat)
+        (api/send-text token id "Vega initialized.")))
 
-  (println "Starting Vega...")
-  (<!! (polling/start token handler
-                      {:timeout 4})))
+    (command-fn "help"
+      (fn [{{id :id :as chat} :chat}]
+        (timbre/info "Help was requested in " chat)
+        (api/send-text token id "No.")))
+
+    (command-fn "time"
+      (fn [{:keys [text chat]}]
+        (api/send-text token (:id chat) (friend-time db-setup
+                                                     (second (s/split text #" "))))))
+
+    (message-fn
+        (fn [{:keys [text chat]}]
+          (when (s/includes? (s/lower-case text) "this is the way")
+            (api/send-text token (:id chat) "This is the way."))))
+
+    (message-fn
+      (fn [message]
+        (println "Intercepted message: " message)
+        (println "Not doing anything with this message.")))))
+
+(defmethod ig/init-key :telegram/polling [_ {:keys [handler
+                                                    token]}]
+  (fn []
+    (timbre/info "Starting Vega...")
+    (polling/start token handler
+                  {:timeout 4})))
+
+(defn start [_]
+  (let [{:telegram/keys [polling]} (ig/init config)]
+    (<!! (polling))))
 
 (defn -main
   []
--- test/vega/core_test.clj
+++ test/vega/core_test.clj
@@ -7,17 +7,24 @@
 (def current-time (t/zoned-date-time (t/local-date-time 2021 1 16 19 0)
                                      default-zone))
 
+(def ^:private test-config
+  (update-in config
+             [:db/config :store]
+             assoc
+             :backend :mem
+             :id      "test-vegadb"))
+
 (deftest friend-time-test
   (with-redefs [now (constantly current-time)]
 
-    (ig/init config)
+    (let [{:db/keys [setup]} (ig/init test-config)]
 
-    (testing "Find friends in Brazil"
-      (is (= "19:00" (friend-time "caio")))
-      (is (= "19:00" (friend-time "bruno"))))
+      (testing "Find friends in Brazil"
+        (is (= "19:00" (friend-time setup "caio")))
+        (is (= "19:00" (friend-time setup "bruno"))))
 
-    (testing "Find friends in Portugal"
-      (is (= "22:00" (friend-time "thiago"))))
+      (testing "Find friends in Portugal"
+        (is (= "22:00" (friend-time setup "thiago"))))
 
-    (testing "Find friends in Germany"
-      (is (= "23:00" (friend-time "pedrotti"))))))
+      (testing "Find friends in Germany"
+        (is (= "23:00" (friend-time setup "pedrotti")))))))
